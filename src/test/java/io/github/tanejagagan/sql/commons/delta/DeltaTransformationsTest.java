4c0e26d436829ae4d6bb057d3fc22eb0256a5957
Shanghao <21551021@zju.edu.cn>
2025-04-07 00:01:47 -0700

    add Delta Lake support

--- src/test/java/io/github/tanejagagan/sql/commons/delta/DeltaTransformationsTest.java
+++ src/test/java/io/github/tanejagagan/sql/commons/delta/DeltaTransformationsTest.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 public class DeltaTransformationsTest {
     @Test
     public void testTransformation() throws SQLException, JsonProcessingException {
-        String sql = "select a, b from t where x = 10 and y = cast('2025-01-01' as date)";
+        String sql = "select a, b from t where x = 10 or y = cast('2025-01-01' as date)";
         JsonNode tree = Transformations.parseToTree(sql);
         ArrayNode statements = (ArrayNode)tree.get("statements");
         JsonNode firstStatement =  statements.get(0);
